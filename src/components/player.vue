245b3cf5c9d55aff6ec6bb6970c9c3283be13423
Esther Witbeck <esther.witbeck@gmail.com>
2023-06-12 09:24:17 -0700

    add conditional input for final

--- src/components/player.vue
+++ src/components/player.vue
@@ -3,8 +3,10 @@ import { ref, computed } from 'vue';
 import type { Ref } from 'vue';
 import type { Player } from '../types';
 import { usePlayerStore } from '../stores/playerSlice';
+import { useGameStore } from '../stores/gameSlice';
 
 const { players, setPlayerGuessStatus } = usePlayerStore();
+const { boardState } = useGameStore();
 
 const props = defineProps({
   player: Object
@@ -21,22 +23,36 @@ const handleIncorrect = id => {
   else setPlayerGuessStatus.value('incorrect', id);
 }
 
+// computed for legal state for correct/not
+const inputting = computed(() => (boardState.value === 'reading' ||
+                                 boardState.value === 'final'));
+
 </script>
 
 <template>
-  <div class="player">
+  <div :class="{
+    player: true,
+    reading: boardState === 'reading',
+    final: boardState === 'final'
+  }">
     <button
+      v-if="inputting"
       @click="() => handleCorrect(props.player.id)"
       :class="{ correct: status === 'correct' }"
     >
       &check;
     </button>
     <button
+      v-if="inputting"
       @click="() => handleIncorrect(props.player.id)"
       :class="{ incorrect: status === 'incorrect' }"
     >
       x
     </button>
+    <input
+      v-if="boardState === 'final'"
+      type="number"
+    >
     <p>${{ props.player.winnings }}</p>
     <p>{{ props.player.name }}</p>
   </div>
@@ -46,7 +62,7 @@ const handleIncorrect = id => {
 .player {
   display: grid;
   gap: 5px;
-  grid-template-columns: 1fr 1fr 2fr 4fr;
+  grid-template-columns: 1fr 1fr; /* grid template names? */
   align-items: center;
   padding: 5px;
   p {
@@ -66,4 +82,14 @@ const handleIncorrect = id => {
 .player:nth-of-type(even) {
   background-color: lightgrey;
 }
+
+.player.reading {
+  grid-template-columns: 1fr 1fr 2fr 4fr;
+}
+
+.player.final {
+  grid-template-columns: 1fr 1fr 2fr 2fr 4fr;
+}
+
+
 </style>
